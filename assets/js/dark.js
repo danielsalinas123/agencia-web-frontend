f1b52438b28d5e5f0fe53bdb20dd6cbcd4158603
Daniel Salinas Jaramillo <leinad.salinsj@gmail.com>
2026-01-29 00:00:54 -0500

    feat(ui): implementar modo oscuro con CSS

---
+++ assets/js/dark.js
@@ -0,0 +1,27 @@
+const switcher=document.querySelector(".switcher__btn");
+const light=document.querySelector(".switcher__icon-light");
+const dark=document.querySelector(".switcher__icon-dark");
+
+switcher.addEventListener("click",()=>
+{
+    const head=document.head;
+
+    const link=document.createElement("link");
+    link.rel="stylesheet";
+    link.href="./assets/css/dark.css";
+    link.id="theme-dark";
+
+    const theme_dark=document.querySelector("#theme-dark");
+    if(theme_dark)
+    {
+        head.removeChild(theme_dark);
+        dark.style.display="none";
+        light.style.display="block";
+    }
+    else
+    {
+        head.appendChild(link);
+        dark.style.display="block";
+        light.style.display="none";
+    }
+});
